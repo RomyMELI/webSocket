7f0c419a073c3dc0dd1597b29ea2a4858bfb7b62
romina corsiglia <rcorsiglia@ar0c02fq4t9q05n.ml.com>
2023-02-06 17:56:53 -0300

    add handlers, home.jet and main.go

---
+++ cmd/web/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"ws/internal/handlers"
+)
+
+func main() {
+	mux := routes()
+
+	log.Println("Starting channel Listener")
+
+	go handlers.ListenToWsChannel()
+
+	log.Println("Starting web server on port 8080")
+
+	_ = http.ListenAndServe(":8080", mux)
+
+}
